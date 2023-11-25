d592275cdb84bc1ceb4f182d9b38ffa46cf8a053
Victor A. <52110451+cs50victor@users.noreply.github.com>
2023-11-25 18:21:21 -0500

    refactor: setup bevy

--- lkgpt/src/main.rs
+++ lkgpt/src/main.rs
@@ -1582,110 +1582,6 @@ mod scene{
     }
 }
 
-mod routes {
-    use actix_web::web;
-
-    mod health_check {
-        use actix_web::{HttpResponse as Resp, Responder};
-
-        use crate::response::ServerMsg;
-
-        pub async fn handler() -> impl Responder {
-            Resp::Ok().json(ServerMsg::data("OK"))
-        }
-    }
-
-    mod lsdk_webhook {
-        use actix_web::{http::Method, web, HttpRequest, HttpResponse as Resp, Responder};
-        use std::sync::Arc;
-
-        use crate::{
-            response::{CommonResponses, ServerMsg},
-            state::ServerStateMutex,
-            utils,
-            webrtc::TurboLivekitConnector,
-        };
-        use livekit_api::{
-            access_token::{self},
-            webhooks,
-        };
-        use log::info;
-
-        pub async fn handler(
-            req: HttpRequest,
-            server_data: web::Data<ServerStateMutex>,
-            body: web::Bytes,
-        ) -> impl Responder {
-            if req.method().ne(&Method::POST) {
-                return Resp::MethodNotAllowed().json(CommonResponses::MethodNotAllowed.json());
-            }
-            let token_verifier = match access_token::TokenVerifier::new() {
-                Ok(i) => i,
-                Err(e) => return Resp::InternalServerError().json(ServerMsg::error(e.to_string())),
-            };
-            let webhook_receiver = webhooks::WebhookReceiver::new(token_verifier);
-
-            let jwt = req
-                .headers()
-                .get("Authorization")
-                .and_then(|hv| hv.to_str().ok())
-                .unwrap_or_default()
-                .to_string();
-
-            let body = match std::str::from_utf8(&body) {
-                Ok(i) => i,
-                Err(e) => return Resp::BadRequest().json(ServerMsg::error(e.to_string())),
-            };
-
-            let event = match webhook_receiver.receive(body, &jwt) {
-                Ok(i) => i,
-                Err(e) => return Resp::InternalServerError().json(ServerMsg::error(e.to_string())),
-            };
-
-            if event.room.is_some() && event.event == "room_started" {
-                info!("ROOM STARTED 🎉");
-                let livekit_protocol::Room {
-                    name: participant_room_name,
-                    max_participants,
-                    num_participants,
-                    ..
-                } = event.room.unwrap();
-
-                if num_participants < max_participants {
-                    let mut turbo_webrtc =
-                        match TurboLivekitConnector::new(participant_room_name).await {
-                            Ok(turbo_webrtc) => turbo_webrtc,
-                            Err(e) => {
-                                return Resp::InternalServerError()
-                                    .json(ServerMsg::error(format!("{e}")))
-                            }
-                        };
-
-                    let mut server_data = server_data.lock();
-                    server_data.turbo_input_tx = Some(turbo_webrtc.get_txt_input_sender());
-                    server_data.turbo_livekit_connector_handle = Some(turbo_webrtc);
-
-                    info!("\nSERVER FINISHED PROCESSING ROOM_STARTED WEBHOOK");
-                };
-            } else {
-                info!("received event {}", event.event);
-            }
-
-            Resp::Ok().json(ServerMsg::data("Livekit Webhook Successfully Processed"))
-        }
-
-        //  images will be in base64
-        // stt & images & text go in -> [find a way of batching all this information and sending it to GPT ] -> stream the response from OPENAI to livekit
-
-        // IT SHOULD NEVER TEXT & WRITE AT the same time
-    }
-
-    pub fn top_level_routes(cfg: &mut web::ServiceConfig) {
-        cfg.service(web::scope("/").service(web::resource("").to(health_check::handler)))
-            .service(web::resource("/lsdk-webhook").to(lsdk_webhook::handler));
-    }
-}
-
 mod utils {
     mod create_bot_token {
         use livekit_api::access_token::{AccessToken, VideoGrants};
@@ -2948,6 +2844,7 @@ mod webrtc {
 
     use anyhow::Result;
     use async_openai::{config::OpenAIConfig, Client as OPENAI_CLIENT};
+    use bevy::app::{App as BevyApp};
     use livekit::{publication::LocalTrackPublication, Room};
 
     use livekit as lsdk;
@@ -2972,7 +2869,6 @@ mod webrtc {
     pub struct TurboLivekitConnector {
         room: Arc<Room>,
         text_input_tx: tokio::sync::mpsc::UnboundedSender<String>,
-        cmd_input_sender: std::sync::mpsc::Sender<String>,
         room_event_handle: JoinHandle<Result<()>>,
         video_pub: LocalTrackPublication,
         audio_pub: LocalTrackPublication,
@@ -2984,6 +2880,8 @@ mod webrtc {
 
     impl TurboLivekitConnector {
         pub async fn new(participant_room_name: String) -> Result<Self> {
+            let bevy_thread_handle = std::thread::spawn(||BevyApp::new().run());
+
             // ************** REQUIRED ENV VARS **************
             let open_ai_org_id = std::env::var("OPENAI_ORG_ID").expect("OPENAI_ORG_ID must be");
             let lvkt_url = std::env::var("LIVEKIT_WS_URL").expect("LIVEKIT_WS_URL is not set");
@@ -2999,7 +2897,6 @@ mod webrtc {
             let room = Arc::new(room);
 
             // ************** CREATE MESSAGING CHANNELS **************
-            let (cmd_input_sender, cmd_input_receiver) = std::sync::mpsc::channel::<String>();
             let (gpt_input_tx, gpt_input_rx) = tokio::sync::mpsc::unbounded_channel::<String>();
             let (to_voice_tx, from_gpt_rx) = tokio::sync::mpsc::unbounded_channel::<String>();
 
@@ -3047,7 +2944,6 @@ mod webrtc {
                 audio_pub,
                 video_pub,
                 room_event_handle,
-                cmd_input_sender,
                 gpt_thread_handle,
                 render_thread_handle: Some(render_thread_handle),
             })
@@ -3080,7 +2976,8 @@ mod webrtc {
     unsafe impl Sync for TurboLivekitConnector {}
 }
 
-// ACTIX SERVER
+// ********* ACTIX SERVER *********
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     // tracing_subscriber::fmt::init();
@@ -3118,4 +3015,111 @@ async fn main() -> std::io::Result<()> {
     .bind(("0.0.0.0", port))?
     .run()
     .await
+}
+
+// server routes
+
+
+mod routes {
+    use actix_web::web;
+
+    mod health_check {
+        use actix_web::{HttpResponse as Resp, Responder};
+
+        use crate::response::ServerMsg;
+
+        pub async fn handler() -> impl Responder {
+            Resp::Ok().json(ServerMsg::data("OK"))
+        }
+    }
+
+    mod lsdk_webhook {
+        use actix_web::{http::Method, web, HttpRequest, HttpResponse as Resp, Responder};
+        use std::sync::Arc;
+
+        use crate::{
+            response::{CommonResponses, ServerMsg},
+            state::ServerStateMutex,
+            utils,
+            webrtc::TurboLivekitConnector,
+        };
+        use livekit_api::{
+            access_token::{self},
+            webhooks,
+        };
+        use log::info;
+
+        pub async fn handler(
+            req: HttpRequest,
+            server_data: web::Data<ServerStateMutex>,
+            body: web::Bytes,
+        ) -> impl Responder {
+            if req.method().ne(&Method::POST) {
+                return Resp::MethodNotAllowed().json(CommonResponses::MethodNotAllowed.json());
+            }
+            let token_verifier = match access_token::TokenVerifier::new() {
+                Ok(i) => i,
+                Err(e) => return Resp::InternalServerError().json(ServerMsg::error(e.to_string())),
+            };
+            let webhook_receiver = webhooks::WebhookReceiver::new(token_verifier);
+
+            let jwt = req
+                .headers()
+                .get("Authorization")
+                .and_then(|hv| hv.to_str().ok())
+                .unwrap_or_default()
+                .to_string();
+
+            let body = match std::str::from_utf8(&body) {
+                Ok(i) => i,
+                Err(e) => return Resp::BadRequest().json(ServerMsg::error(e.to_string())),
+            };
+
+            let event = match webhook_receiver.receive(body, &jwt) {
+                Ok(i) => i,
+                Err(e) => return Resp::InternalServerError().json(ServerMsg::error(e.to_string())),
+            };
+
+            if event.room.is_some() && event.event == "room_started" {
+                info!("ROOM STARTED 🎉");
+                let livekit_protocol::Room {
+                    name: participant_room_name,
+                    max_participants,
+                    num_participants,
+                    ..
+                } = event.room.unwrap();
+
+                if num_participants < max_participants {
+                    let mut turbo_webrtc =
+                        match TurboLivekitConnector::new(participant_room_name).await {
+                            Ok(turbo_webrtc) => turbo_webrtc,
+                            Err(e) => {
+                                return Resp::InternalServerError()
+                                    .json(ServerMsg::error(format!("{e}")))
+                            }
+                        };
+
+                    let mut server_data = server_data.lock();
+                    server_data.turbo_input_tx = Some(turbo_webrtc.get_txt_input_sender());
+                    server_data.turbo_livekit_connector_handle = Some(turbo_webrtc);
+
+                    info!("\nSERVER FINISHED PROCESSING ROOM_STARTED WEBHOOK");
+                };
+            } else {
+                info!("received event {}", event.event);
+            }
+
+            Resp::Ok().json(ServerMsg::data("Livekit Webhook Successfully Processed"))
+        }
+
+        //  images will be in base64
+        // stt & images & text go in -> [find a way of batching all this information and sending it to GPT ] -> stream the response from OPENAI to livekit
+
+        // IT SHOULD NEVER TEXT & WRITE AT the same time
+    }
+
+    pub fn top_level_routes(cfg: &mut web::ServiceConfig) {
+        cfg.service(web::scope("/").service(web::resource("").to(health_check::handler)))
+            .service(web::resource("/lsdk-webhook").to(lsdk_webhook::handler));
+    }
 }
