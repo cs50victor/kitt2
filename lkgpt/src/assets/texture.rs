7b9073b5242f9bb8ef115636bb83cf05e26056b4
Victor A. <52110451+cs50victor@users.noreply.github.com>
2023-11-25 15:38:54 -0500

    refactor: multi-file mods to single file

--- lkgpt/src/assets/texture.rs
+++
@@ -1,8 +0,0 @@
-use std::sync::Arc;
-
-use vulkano::image::{view::ImageView, ImmutableImage};
-
-#[derive(Clone, Debug)]
-pub struct Texture {
-    pub image_view: Arc<ImageView<ImmutableImage>>,
-}
